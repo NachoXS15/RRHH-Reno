23bbf5f9e74d43fe393dbd30e4cdbd09360aa1f8
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-04-17 10:14:21 -0300

    updated db credentials using env

--- conection.php
+++ conection.php
@@ -1,6 +1,11 @@
 <?php
+    $host = getenv("DB_HOST");
+    $user = getenv("DB_USER");
+    $pass = getenv("DB_PASS");
+    $db = getenv("DB_NAME");
     try {
-        $connect = new mysqli("localhost","root","","renovacion");
+        $connect = new mysqli($host, $user, $pass, $db);
+        echo "Conexión exitosa";
     } catch (\Throwable $th) {
         echo "Error al conectar". $th->getMessage();
     }
