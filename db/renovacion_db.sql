9b5450603a468ce371016c0e71436413abd1f743
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-04-22 11:54:56 -0300

    upload db

--- db/renovacion_db.sql
+++ db/renovacion_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-04-2024 a las 17:48:20
+-- Tiempo de generación: 22-04-2024 a las 16:54:14
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -17,6 +17,32 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `renovacion_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rrhh_dl`
+--
+
+CREATE TABLE `rrhh_dl` (
+  `id` int(11) NOT NULL,
+  `antiguedad` varchar(10) NOT NULL,
+  `condicion` varchar(30) NOT NULL,
+  `estudios` varchar(40) NOT NULL,
+  `area` varchar(30) NOT NULL,
+  `persona_a_cargo` varchar(50) NOT NULL,
+  `Disponibilidad` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rrhh_dl`
+--
+
+INSERT INTO `rrhh_dl` (`id`, `antiguedad`, `condicion`, `estudios`, `area`, `persona_a_cargo`, `Disponibilidad`) VALUES
+(1, '2024', 'Beca', 'Secundario Compleo', 'Formadores Deportivos', 'Maximiliano Guia', 'Lunes, Miercoles y Viernes');
 
 -- --------------------------------------------------------
 
@@ -42,11 +68,19 @@ CREATE TABLE `rrhh_dp` (
 INSERT INTO `rrhh_dp` (`id`, `nombre_completo`, `dni`, `nacimiento`, `telefono`, `hijos`, `domicilio`, `observaciones`) VALUES
 (1, 'Ignacio Pantoja', 44008496, '2002-05-03', 3804800454, 0, 'Guaranies 1174', 'Perteneciente al equipo de Maxi'),
 (3, 'Maximiliano Elias Guia Roja', 39298276, '1996-01-15', 3804482823, 0, 'Barrio Legislativo', 'un capo'),
-(7, 'Cristian Pugea', 123123123, '2024-04-10', 38041231231, 1, 'Barrio Hospital', 'el + kapito');
+(7, 'Cristian Pugea', 123123123, '2024-04-10', 38041231231, 1, 'Barrio Hospital', 'el + kapito'),
+(8, 'Maira Fabiana Ortega', 42843635, '2001-03-07', 3804717880, 0, 'Barrio Ginobili', 'Representante del equipo de Maxi'),
+(9, 'Joaquìn Giordano', 47304786, '2003-09-09', 3804151587, 1, 'Barrio Federaciòn', 'Un capo');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `rrhh_dl`
+--
+ALTER TABLE `rrhh_dl`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `rrhh_dp`
@@ -59,10 +93,26 @@ ALTER TABLE `rrhh_dp`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `rrhh_dl`
+--
+ALTER TABLE `rrhh_dl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `rrhh_dp`
 --
 ALTER TABLE `rrhh_dp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `rrhh_dl`
+--
+ALTER TABLE `rrhh_dl`
+  ADD CONSTRAINT `rrhh_dl_ibfk_1` FOREIGN KEY (`id`) REFERENCES `rrhh_dp` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
